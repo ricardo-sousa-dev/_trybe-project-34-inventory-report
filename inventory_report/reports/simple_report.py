7fd251e5e5cb67498e3d5e5b6e998051b76951a6
ricardo-sousa <rwmsousa@gmail.com>
2022-03-31 17:49:52 -0300

    feat: requirement 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from statistics import mode
+
+
+class SimpleReport():
+    def generate(dict):
+        date_fab = [items["data_de_fabricacao"] for items in dict]
+        date_now = str(datetime.now().today())
+        date_val = [items["data_de_validade"] for items in dict
+                    if items["data_de_validade"] > date_now]
+        big_company = [items["nome_da_empresa"] for items in dict]
+
+        return (
+            f"Data de fabricação mais antiga: {min(date_fab)}\n"
+            f"Data de validade mais próxima: {min(date_val)}\n"
+            "Empresa com maior quantidade de produtos estocados: "
+            f"{mode(big_company)}\n"
+        )
+
+# referencies:
+# https://www.geeksforgeeks.org/python-statistics-mode-function/
+# https://pythonacademy.com.br/blog/list-comprehensions-no-python
