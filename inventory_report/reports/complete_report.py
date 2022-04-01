f60fd7f0b32e810d38f49112b48ce109c1ddd177
ricardo-sousa <rwmsousa@gmail.com>
2022-04-01 10:10:29 -0300

    feat - requirement 2

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,19 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport():
+    def generate(dict):
+        simple_report = SimpleReport.generate(dict)
+        companies = [items["nome_da_empresa"] for items in dict]
+
+        list_companies = ""
+
+        for company in Counter(companies).items():
+            list_companies += f"- {company[0]}: {company[1]}\n"
+
+        return (
+            f"{simple_report}\n" +
+            "\nProdutos estocados por empresa: \n" +
+            list_companies
+        )
